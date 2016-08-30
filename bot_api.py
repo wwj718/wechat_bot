d5d764cfde7261ac938ef275b4e1202231505017
wwj718 <wuwenjie718@gmail.com>
2016-08-30 11:57:54 +0800

    Update bot_api.py

--- bot_api.py
+++ bot_api.py
@@ -9,6 +9,7 @@ import hug
 deepThought = ChatBot("deepThought")
 deepThought.set_trainer(ChatterBotCorpusTrainer)
 # 使用中文语料库训练它
+# 只需要训练一次，不需要每次启动进程都训练，训练结果默认存到本地`./database.db`,之后启动进程会使用这个数据库
 deepThought.train("chatterbot.corpus.chinese")  # 语料库
 
 
